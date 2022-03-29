2040b04d64d12e6b05270764ed6561992a74471a
Artem Travlo <info.travlo@gmail.com>
2022-03-29 22:07:38 +0300

    Add cors and testing

--- client/src/CreatePost.js
+++ client/src/CreatePost.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function App() {
+  const [title, setTitle] = useState("");
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    await axios.post("http://localhost:5000/posts/", {
+      title,
+    });
+
+    setTitle("");
+  }
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <label htmlFor="post">Create post</label>
-        <input type="text" name="post" />
-        <button>Create</button>
+        <input
+          type="text"
+          name="post"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Create</button>
       </form>
     </div>
   );
