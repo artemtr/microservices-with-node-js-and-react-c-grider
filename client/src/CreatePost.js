2bc8c0f6133120c8207aefd3d885361b5e983b81
Artem Travlo <info.travlo@gmail.com>
2022-03-29 18:30:33 +0300

    error with child

---
+++ client/src/CreatePost.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="post">Create post</label>
+        <input type="text" name="post" />
+        <button>Create</button>
+      </form>
+    </div>
+  );
+}
