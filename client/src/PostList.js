caeea952a6a33647e76fce24d1a1359b3bdb6d19
Artem Travlo <info.travlo@gmail.com>
2022-03-30 16:00:25 +0300

    Add comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommentCreate from './CommentCreate';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentsList from "./ComentsList";
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
+    const res = await axios.get("http://localhost:4000/posts");
 
     setPosts(res.data);
   };
@@ -15,15 +16,16 @@ export default () => {
     fetchPosts();
   }, []);
 
-  const renderedPosts = Object.values(posts).map(post => {
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
         className="card"
-        style={{ width: '30%', marginBottom: '20px' }}
+        style={{ width: "30%", marginBottom: "20px" }}
         key={post.id}
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentsList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
