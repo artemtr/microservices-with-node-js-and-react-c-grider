f984444f5decdb465f1f876af0fd8739d072e552
Artem Travlo <info.travlo@gmail.com>
2022-04-02 16:11:06 +0300

    Add event bus

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
-import CommentsList from "./ComentsList";
 
 export default () => {
   const [posts, setPosts] = useState({});
+  const [comments, setComments] = useState({});
 
   const fetchPosts = async () => {
     const res = await axios.get("http://localhost:4000/posts");
@@ -12,11 +12,30 @@ export default () => {
     setPosts(res.data);
   };
 
+  const fetchedComments = async () => {
+    const comments = await axios.get(`http://localhost:4001/posts/comments`);
+    setComments(comments.data);
+  };
+
   useEffect(() => {
     fetchPosts();
   }, []);
 
+  useEffect(() => {
+    fetchedComments();
+  }, []);
+
   const renderedPosts = Object.values(posts).map((post) => {
+    const commentsForPost = comments[post.id] || [];
+
+    const renderedComments = commentsForPost.map((comment) => {
+      return (
+        <div>
+          <li key={comment.id}>{comment.content}</li>
+        </div>
+      );
+    });
+
     return (
       <div
         className="card"
@@ -25,7 +44,7 @@ export default () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentsList postId={post.id} />
+          {renderedComments}
           <CommentCreate postId={post.id} />
         </div>
       </div>
